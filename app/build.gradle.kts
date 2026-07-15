d92a9d5481fc2d0e31fd179997614a30a4990e09
sunuoy <153935091+sunuoy@users.noreply.github.com>
2026-07-15 23:20:33 +0530

    perf: change R8 configuration to low-level optimizations by disabling minification and resource shrinking for fast release builds

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -107,8 +107,8 @@ signingConfigs {
   buildTypes {
     release {
       isCrunchPngs = true
-      isMinifyEnabled = true
-      isShrinkResources = true
+      isMinifyEnabled = false
+      isShrinkResources = false
       proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
       signingConfig = signingConfigs.getByName("release")
     }
